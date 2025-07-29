87f6c36c6611f32adcd20f231d41530f6bbca3ae
roman.vakulenko <mail@vacoola.neme>
2025-07-29 07:36:03 -0500

    [BL-20] testcontainers again

--- src/test/java/com/bmlab/launchpad/LaunchpadApplicationTests.java
+++ src/test/java/com/bmlab/launchpad/LaunchpadApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 
-//@Import(TestcontainersConfiguration.class)
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class LaunchpadApplicationTests {
 
